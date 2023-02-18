2ddaf868f93437598c4d3443aa59bf8ff24022bf
Shreeram214 <shreeramhankare214@gmail.com>
2023-02-18 10:52:39 +0530

    New controller

---
+++ GitEcommerce/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Optimization;
+
+namespace GitEcommerce.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
